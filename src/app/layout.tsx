95f0df4a9e7624c6c9bd2601a2452582fd39d11d
kaio <kaitozhiroki@gmail.com>
2026-07-26 18:17:50 -0300

    refactor: adjusting layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${notoSerif} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" className={`${notoSerif}`}>
+      <body className="">{children}</body>
     </html>
   );
 }
